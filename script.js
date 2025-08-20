974732cf62c662f468777288dc23b823b2917c8a
Ant-Hall <ahall944@west-mec.org>
2025-08-19 18:24:51 -0700

    scrims

---
+++ script.js
@@ -0,0 +1,82 @@
+// let name = "Anthony Hall"
+// let vacation = "Travelling Vacation"
+// console.log(name)
+// alert("Hello Strangler")
+// console.log(confirm("Confirm you are alive?"))
+// let answer1 = prompt("How old are you right now?")
+// console.log(answer1)
+// if(answer1 == 5){
+//     console.log("Get off the internet pretty please")
+// }else{
+//     console.log("Aight")
+// }
+
+// alert("Quiz begins")
+
+// // let answer2 = prompt("whats 3+3")
+// // console.log(answer2)
+// // if(answer2 == 6){
+// //     console.log("Cool, thats great.")
+// // }else{
+// //     console.log("Incorrect")
+// // }
+
+// // let answer3 = prompt("Did you answer the prior question?")
+// // console.log(answer3)
+// // if (answer3 == "yes"){
+// //     console.log("Awesome.")
+// // }else{
+// //     console.log("<>.<>")
+// // }
+// // let answer4 = prompt("In 60 seconds how many minutes pass in Africa)
+// // console.log(answer4)
+// // if answer
+// let kitchen = "spoon"
+// kitchen = "fork"
+// kitchen = "spork"
+// console.log(kitchen)
+// const LastName = "Hall"
+// // LastName = "Titus"
+// console.log(LastName)
+
+// let a, b, c;
+// a, b, c = 5
+// a = 9; //a = 9
+// b = b + 3 // b = 8
+// c = b - 2 // c = 6
+// b = a; // b = 9
+
+
+// console.log(typeof("true"))
+// console.log(typeof(65))
+// console.log(typeof(false))
+// console.log(typeof(undefined))
+// console.log(typeof(null))
+// console.log(typeof(Math))
+// console.log(typeof(alert()))
+
+// let y="2"
+// if( y==2){
+//     console.log("yes")
+// }
+// // 41 correct
+// // 232 correct
+// // 19 
+// // 2
+// // 57 >> 543
+// 2 + 2 + '1'
+// 2 + '3' + '2'
+// 2 - '1' + '9'
+// '6'-'3'
+
+let test = 4;
+console.log("the value of the test is:" + test + "!");
+
+let a,b,c;
+a='4512585'
+b='false'
+c=true
+console.log(typeof(string(c)))
+console.log(typeof(boolean(b)))
+console.log(typeof(number(a)))
+
